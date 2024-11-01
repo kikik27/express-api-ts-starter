ff0c279e965bc50927ddae0ee35531b9dabdfff6
M.ZAKY ARISANDHI <mkikik27@gmail.com>
2024-11-01 14:25:36 +0700

    add (schema): add user schema

---
+++ src/db/schema/users.ts
@@ -0,0 +1,14 @@
+import { pgTable, integer, uuid, varchar, pgEnum } from "drizzle-orm/pg-core";
+import { globalSchema } from "./global.schema";
+
+export const rolesEnum = pgEnum("roles", ["admin", "user"])
+
+export const users = pgTable("users", {
+  name: varchar("name", { length: 256 }).notNull(),
+  email: varchar("email", { length: 256 }).notNull(),
+  password: varchar("password", { length: 256 }).notNull(),
+  role: rolesEnum().default('user'),
+  ...globalSchema
+});
+
+export type User = typeof users.$inferSelect;
