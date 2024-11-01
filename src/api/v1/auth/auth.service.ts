d055a330e6d46469b716480b9340d7f29886e190
M.ZAKY ARISANDHI <mkikik27@gmail.com>
2024-11-01 17:38:34 +0700

    add (service): add auth services

--- src/api/v1/auth/auth.service.ts
+++ src/api/v1/auth/auth.service.ts
@@ -2,10 +2,31 @@ import { db } from "../../../db";
 import { users } from "../../../db/schema/users";
 import { User } from "../../../db/schema/users";
 import bcrypt from 'bcrypt';
+import { generateAccessToken, generateTokens } from "../../../utils/jwt";
+import { eq } from "drizzle-orm";
+import { log } from "console";
+import { createUser } from "../users/user.service";
 
 export const register = async (user: User) => {
-  const { password, ...rest } = user;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = await db.insert(users).values({ ...rest, password: hashedPassword });
-  return newUser;
+  const newUser = await createUser(user);
+  return generateTokens(user);
 } 
+
+export const login = async (payload: User) => {
+  try {
+    const { password, ...rest } = payload;
+    const user = await db.select().from(users).where(eq(users.email, rest.email)).limit(1);
+    console.log(user);
+    if (!user) {
+    throw new Error("User not found");
+  }
+  const isPasswordValid = await bcrypt.compare(password, user[0].password);
+  if (!isPasswordValid) {
+    throw new Error("Invalid password");
+  }
+    const { accessToken } = await generateTokens(user[0]);
+    return { accessToken };
+  } catch (error) {
+    throw error;
+  }
+}
