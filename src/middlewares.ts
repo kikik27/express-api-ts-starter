7a2c5a67327340e3cbe9cadca31dbf0b1bd572a7
M.ZAKY ARISANDHI <mkikik27@gmail.com>
2024-11-01 14:41:05 +0700

    add (middleware): add ZOD validation data

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { ErrorResponse } from './interfaces/response.interface';
 import jwt from 'jsonwebtoken';
+import { z } from 'zod';
 
 export const notFound = (req: Request, res: Response, next: NextFunction) => {
   res.status(404);
-  const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
+  const error = new Error(`Not Found - ${req.originalUrl}`);
   next(error);
 }
 
@@ -34,8 +35,28 @@ export const isAuthenticate = (req: Request, res: Response, err: Error, next: Ne
     if (err === "TokenExpiredError") {
       throw new Error(err);
     }
-    throw new Error("🚫 Un-Authorized 🚫");
+    throw new Error("Un-Authorized");
   }
 
   return next();
+}
+
+export const validateRequest = (schema: z.ZodSchema<any, any>) => (req: Request, res: Response, next: NextFunction) => {
+try {
+  schema.parse(req.body);
+  next();
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      const errorMessages = error.errors.map((issue: any) => ({
+        message: `${issue.path.join(".")} is ${issue.message}`,
+    }));
+      res
+        .status(400)
+        .json({ message: "Validation failed", details: errorMessages });
+    } else {
+      res
+        .status(500)
+        .json({ message: "Internal Server Error" });
+    }
+}
 }
