bd23753773dad6b4f408d12617aef37bc69e1095
M.ZAKY ARISANDHI <mkikik27@gmail.com>
2024-11-01 14:41:40 +0700

    add (validation): add validation payload

--- src/api/v1/auth/auth.controller.ts
+++ src/api/v1/auth/auth.controller.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { register } from "./auth.service";
+import { userRegistrationSchema } from "./user.schema";
+import { validateRequest } from "../../../middlewares";
 
 const router = express.Router();
 
-router.post('/register', register);
+router.post('/register', validateRequest(userRegistrationSchema), register);
 
 export default router;
