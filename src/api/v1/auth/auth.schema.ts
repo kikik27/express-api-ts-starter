ed3f69c4b0734b4d0e214ffca962635f689bc9b8
M.ZAKY ARISANDHI <mkikik27@gmail.com>
2024-11-01 17:36:40 +0700

    add (interface): add interface for user profile

--- src/api/v1/auth/auth.schema.ts
+++ src/api/v1/auth/auth.schema.ts
@@ -10,3 +10,10 @@ export const userLoginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
 });
+
+export interface UserCredentials {
+  id: string;
+  name: string;
+  email: string;
+  role: string | null | undefined;
+}
