22f5d6463213595e61169b015b057a4139c57cc7
M.ZAKY ARISANDHI <mkikik27@gmail.com>
2024-11-01 17:40:29 +0700

    add (schma): add auto generate uuid

--- src/db/schema/global.schema.ts
+++ src/db/schema/global.schema.ts
@@ -1,7 +1,8 @@
+import { sql } from "drizzle-orm";
 import { timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const globalSchema = {
-  id: uuid().primaryKey(),
+  id: uuid().primaryKey().default(sql`gen_random_uuid()`),
   updated_at: timestamp(),
   created_at: timestamp().defaultNow().notNull(),
   deleted_at: timestamp(),
