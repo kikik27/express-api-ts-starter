4578ccc6875a68307ebc27afabdb83bd3c40b839
M.ZAKY ARISANDHI <mkikik27@gmail.com>
2024-11-01 17:39:51 +0700

    add (controller): add register, login and profile route

--- src/api/v1/auth/auth.controller.ts
+++ src/api/v1/auth/auth.controller.ts
@@ -1,10 +1,31 @@
 import express from "express";
-import { register } from "./auth.service";
-import { userRegistrationSchema } from "./user.schema";
-import { validateRequest } from "../../../middlewares";
+import { login, register } from "./auth.service";
+import { userLoginSchema, userRegistrationSchema } from "./auth.schema";
+import { isAuthenticate, validateRequest } from "../../../middlewares";
+import { createResponses } from "../../../helpers/response";
 
 const router = express.Router();
+const response = createResponses('auth')
 
-router.post('/register', validateRequest(userRegistrationSchema), register);
+router.post('/register', validateRequest(userRegistrationSchema), async (req, res) => {
+  try {
+    const token = await register(req.body);
+    res.status(201).json(response.created(token, "User registered successfully"));
+  } catch (error) {
+    res.status(500).json(response.error(error));
+  }
+});
+router.post('/login', validateRequest(userLoginSchema), async (req, res) => {
+  try {
+    const user = await login(req.body);
+    res.status(200).json(response.fetched(user, "User logged in successfully"));
+  } catch (error) {
+    res.status(500).json(response.error(error));
+  }
+});
+
+router.get('/me', isAuthenticate, async (req, res) => {
+  res.status(200).json(response.fetched(req.user, "User profile fetched successfully"));
+});
 
 export default router;
