6ea8fe88a56a6da3836cf8ac6df010fc37fddfdb
M.ZAKY ARISANDHI <mkikik27@gmail.com>
2024-11-01 17:41:53 +0700

    fix (database): fix DRIZZLE connection client

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,9 @@
+import "dotenv/config";
+
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const client = postgres(process.env.DATABASE_URL as string, { prepare: false });
-export const db = drizzle({ client });
+const DB_URL = process.env.DATABASE_URL;
+
+export const client = postgres(DB_URL!, { prepare: false });
+export const db = drizzle(client);
