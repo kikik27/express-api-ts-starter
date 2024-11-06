016b09b72eb24a78664333e8b2ec11040696170d
M.ZAKY ARISANDHI <mkikik27@gmail.com>
2024-11-06 17:46:31 +0700

    add (auth): add role permission

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { getUserById } from "../api/v1/users/user.service";
 import { UserCredentials } from "../api/v1/auth/auth.schema";
+import { User } from "../db/schema/users";
 
 // Add interface for JWT payload
 interface JWTPayload {
@@ -18,25 +19,28 @@ declare global {
   }
 }
 
-export const isAuthenticate = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const { authorization } = req.headers;
+export const isAuthenticate = (allowedRoles?: User['role'][]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const { authorization } = req.headers;
 
-  if (!authorization?.startsWith("Bearer ")) {
-    res.status(401);
-    throw new Error("Un-Authorized: Bearer token required");
-  }
+    if (!authorization?.startsWith("Bearer ")) {
+      res.status(401);
+      throw new Error("Un-Authorized: Bearer token required");
+    }
 
-  try {
-    const token = authorization.split(" ")[1];
-    const payload = jwt.verify(token, process.env.JWT_SECRET!) as JWTPayload;
-    const user = await getUserById(payload.userId);
-    req.user = user;
-    next();
-  } catch (error) {
-    res.status(401).json({ message: "Un-Authorized: Invalid token" });
-  }
+    try {
+      const token = authorization.split(" ")[1];
+      const payload = jwt.verify(token, process.env.JWT_SECRET!) as JWTPayload;
+      const user = await getUserById(payload.userId);
+
+      if (allowedRoles && !allowedRoles.includes(user.role)) {
+        return res.status(403).json({ message: "Insufficient permissions" });
+      }
+
+      req.user = user;
+      next();
+    } catch (error) {
+      res.status(401).json({ message: "Token invalid or expired" });
+    }
+  };
 }; 
