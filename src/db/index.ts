27677ab41a7eb616eb5a21866fae65b6f635c1ac
M.ZAKY ARISANDHI <mkikik27@gmail.com>
2024-11-07 17:02:33 +0700

    improvement db connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,5 +5,21 @@ import postgres from "postgres";
 
 const DB_URL = process.env.DATABASE_URL;
 
-export const client = postgres(DB_URL!, { prepare: false });
+export const client = postgres(DB_URL!, {
+  max: 10,
+  idle_timeout: 10000,
+  connect_timeout: 10,
+  prepare: false,
+});
+
+process.on("connect", () => {
+  console.log("Connected to the database");
+});
+
+process.on('SIGTERM', async () => {
+  await client.end();
+  console.log('Database pool closed gracefully');
+  process.exit(0);
+});
+
 export const db = drizzle(client);
